1510512d98249bf2d337fec30791b4874382d81a
iliacodes <6ac177ed@protonmail.com>
2023-02-18 15:18:52 -0800

    added color randomness to NFT.

--- contracts/MoonShotNFT.sol
+++ contracts/MoonShotNFT.sol
@@ -9,16 +9,17 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-import { Base64 } from "./libraries/Base64.sol";
-
+import {Base64} from "./libraries/Base64.sol";
 
 contract MoonShotNFT is ERC721URIStorage {
     // keeps track of token ids
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    string baseSvg =
-        "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+    string svgPartOne =
+        "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='";
+    string svgPartTwo =
+        "'/><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
 
     string[] firstWords = ["Send", "it", "higher!"];
 
@@ -26,17 +27,17 @@ contract MoonShotNFT is ERC721URIStorage {
 
     string[] thirdWords = ["Moonshot", "will", "help", "you", "get", "there."];
 
+    string[] colors = ["red", "#08C2A8", "black", "yellow", "blue", "green"];
+
     event NewEpicNFTMinted(address sender, uint256 tokenId);
 
     constructor() ERC721("MoonShotNFT", "MSNFT") {
         console.log("This is my NFT contract. Woah!");
     }
 
-    function pickRandomFirstWord(uint256 tokenId)
-        public
-        view
-        returns (string memory)
-    {
+    function pickRandomFirstWord(
+        uint256 tokenId
+    ) public view returns (string memory) {
         uint256 rand = random(
             string(abi.encodePacked("FIRST_WORD", Strings.toString(tokenId)))
         );
@@ -44,11 +45,9 @@ contract MoonShotNFT is ERC721URIStorage {
         return firstWords[rand];
     }
 
-    function pickRandomSecondWord(uint256 tokenId)
-        public
-        view
-        returns (string memory)
-    {
+    function pickRandomSecondWord(
+        uint256 tokenId
+    ) public view returns (string memory) {
         uint256 rand = random(
             string(abi.encodePacked("SECOND_WORD", Strings.toString(tokenId)))
         );
@@ -56,16 +55,24 @@ contract MoonShotNFT is ERC721URIStorage {
         return secondWords[rand];
     }
 
-    function pickRandomThirdWord(uint256 tokenId)
-        public
-        view
-        returns (string memory)
-    {
+    function pickRandomThirdWord(
+        uint256 tokenId
+    ) public view returns (string memory) {
         uint256 rand = random(
             string(abi.encodePacked("THIRD_WORD", Strings.toString(tokenId)))
         );
         rand = rand % thirdWords.length;
         return thirdWords[rand];
+    }
+
+    function pickRandomColor(
+        uint256 tokenId
+    ) public view returns (string memory) {
+        uint256 rand = random(
+            string(abi.encodePacked("COLOR", Strings.toString(tokenId)))
+        );
+        rand = rand % colors.length;
+        return colors[rand];
     }
 
     function random(string memory input) internal pure returns (uint256) {
@@ -81,9 +88,16 @@ contract MoonShotNFT is ERC721URIStorage {
         string memory combinedWord = string(
             abi.encodePacked(first, second, third)
         );
+        string memory randomColor = pickRandomColor(newItemId);
 
         string memory finalSvg = string(
-            abi.encodePacked(baseSvg, combinedWord, "</text></svg>")
+            abi.encodePacked(
+                svgPartOne,
+                combinedWord,
+                randomColor,
+                svgPartTwo,
+                "</text></svg>"
+            )
         );
 
         // Get all the JSON metadata in place and base64 encode it.
